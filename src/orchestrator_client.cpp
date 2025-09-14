09a1ee638f08127bb16b793595051205b265fa78
gukush <s184339@student.pg.edu.pl>
2025-09-14 17:21:08 +0200

    Add multi head attention strategy

--- src/orchestrator_client.cpp
+++ src/orchestrator_client.cpp
@@ -118,7 +118,7 @@ void OrchestratorClient::register_with_server() {
     // Send client:join message to register with the server
     json capabilities = {
         {"frameworks", {"opencl", "vulkan", "cuda", "exe"}},
-        {"strategies", {"native-block-matmul", "native-block-matmul-flex","exe-block-matmul-flex","native-ecm-stage1","native-distributed-sort"}},
+        {"strategies", {"native-block-matmul", "native-block-matmul-flex","exe-block-matmul-flex","native-ecm-stage1","native-distributed-sort","native-multi-head-attention"}},
         {"capacity", max_concurrency_},
         {"clientType", "native"}
     };
